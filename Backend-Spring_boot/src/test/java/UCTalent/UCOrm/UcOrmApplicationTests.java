55c442c2d324db5da9d3c70c7dbfa1e31ba94941
congnghia2005 <congnghiatran1@gmail.com>
2026-05-20 21:48:07 +0700

    #3 - complete spring boot set up

---
+++ Backend-Spring_boot/src/test/java/UCTalent/UCOrm/UcOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package UCTalent.UCOrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
